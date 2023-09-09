655a929f0495e8b176cf1bb269a57fdd547eb3b3
ahoffmei <ahoffmei@gmail.com>
2023-09-09 14:23:03 -0700

    made text more visible

--- Scripts/exampleScript.py
+++ Scripts/exampleScript.py
@@ -1,1 +1,25 @@
+# This is just a dummy script 
+
 print('hello world')
+
+
+# Bunch of dummy comments to make code more visible 
+
+# Bunch of dummy comments to make code more visible 
+
+# Bunch of dummy comments to make code more visible 
+# Bunch of dummy comments to make code more visible 
+
+
+# Bunch of dummy comments to make code more visible 
+
+# Bunch of dummy comments to make code more visible 
+
+
+# Bunch of dummy comments to make code more visible 
+
+
+# Bunch of dummy comments to make code more visible 
+
+
+# Bunch of dummy comments to make code more visible 
